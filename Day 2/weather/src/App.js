a1d5920f7db769b146802c75114a08e615a6fd47
Pragalbha Patil <pragalbha.patil@gocomet.com>
2022-01-04 20:18:23 +0530

    weather init

---
+++ Day 2/weather/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+export default class App extends Component {
+
+  state = {
+    weatherData = [],
+  };
+
+  render() {
+    return (
+      <>
+        <p>Hello</p>
+      </>
+    );
+  }
+}
